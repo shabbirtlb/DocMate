4c633bc2a005a2c3272e7584718daa745241b8a5
shabbirtlb <46365055+shabbirtlb@users.noreply.github.com>
2025-06-18 18:08:12 +0530

    Updated supabaseClient.ts

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const supabase = createClient(
-  'https://aoozvekvlmbrfrdoqwnu.supabase.co',
-  'public-anon-or-service-role-key'
+  'https://aoozvekvlmbrfrdoqwnu.supabase.co',  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvb3p2ZWt2bG1icmZyZG9xd251Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MDIzMzI2NSwiZXhwIjoyMDY1ODA5MjY1fQ.GaZiYUb4XIpiYy6BRciiP3qO6tR7lM-KQXKSS_P3WpQ'
 );
